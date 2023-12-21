a573d242d51f0ba80588982709bfec9ebd178628
Andrew Dacenko <a.s.dvornik@gmail.com>
2023-12-21 18:43:53 +0000

    day 21 part 2

--- day-21-walking/src/main.rs
+++ day-21-walking/src/main.rs
@@ -6,10 +6,13 @@ mod walker;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
+    let steps = args[2].parse::<usize>().unwrap();
 
     println!("In file {}", file_path);
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let sum = walker::count_tiles(&contents, 64);
     println!("Sum:\n{sum}");
+    let sum_infinite = walker::count_tiles_infinite(&contents, steps);
+    println!("Sum infinite:\n{sum_infinite}");
 }
--- day-21-walking/src/walker.rs
+++ day-21-walking/src/walker.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 #[derive(Clone, Copy, Debug)]
 enum Tile {
@@ -37,29 +37,7 @@ type Grid = Vec<Vec<Tile>>;
 const STEPS: [(i64, i64); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
 
 pub fn count_tiles(input: &str, steps: usize) -> usize {
-    let mut start: Point = Point(0, 0);
-    let grid: Grid = input
-        .lines()
-        .enumerate()
-        .map(|(row, line)| {
-            line.as_bytes()
-                .iter()
-                .enumerate()
-                .inspect(|(col, b)| {
-                    if b.to_owned().eq(&b'S') {
-                        start = Point(row as i64, *col as i64);
-                    }
-                })
-                .map(|(_col, b)| match b {
-                    b'S' => Tile::Start,
-                    b'.' => Tile::Garden,
-                    b'#' => Tile::Rock,
-                    _ => panic!("Unknown tile {}", char::from(*b)),
-                })
-                .collect()
-        })
-        .collect();
-
+    let (start, grid) = parse_input(input);
     let mut visited: HashMap<Point, usize> = HashMap::from([(start.clone(), 0)]);
     let mut stack = vec![start.next(&grid)];
     let mut step = 0;
@@ -86,6 +64,54 @@ pub fn count_tiles(input: &str, steps: usize) -> usize {
         .iter()
         .filter(|(_point, step)| step.to_owned() % 2 == res)
         .map(|(point, _step)| point)
-        .collect::<HashSet<&Point>>()
-        .len();
+        .count();
+}
+
+pub fn count_tiles_infinite(input: &str, steps: usize) -> usize {
+    let grid_size = input.lines().count();
+    let expanded_input = expand_map(input);
+    let expansion = steps / grid_size;
+    let rest = steps - expansion * grid_size;
+
+    let results = (
+        count_tiles(&expanded_input, rest),
+        count_tiles(&expanded_input, rest + grid_size), // expand once
+        count_tiles(&expanded_input, rest + grid_size * 2), // expand twice
+    );
+
+    return interpolate(expansion, results);
+}
+
+fn expand_map(input: &str) -> String {
+    return [input; 5]
+        .join("\n")
+        .lines()
+        .map(|l| [l; 5].join(""))
+        .collect::<Vec<String>>()
+        .join("\n");
+}
+
+fn interpolate(n: usize, results: (usize, usize, usize)) -> usize {
+    let a = (results.2 + results.0 - 2 * results.1) / 2;
+    let b = results.1 - results.0 - a;
+    let c = results.0;
+    return a * n * n + b * n + c;
+}
+
+fn parse_input(input: &str) -> (Point, Grid) {
+    let grid: Grid = input
+        .lines()
+        .map(|line| {
+            line.as_bytes()
+                .iter()
+                .map(|b| match b {
+                    b'S' => Tile::Start,
+                    b'.' => Tile::Garden,
+                    b'#' => Tile::Rock,
+                    _ => panic!("Unknown tile {}", char::from(*b)),
+                })
+                .collect()
+        })
+        .collect();
+    return (Point(grid.len() as i64 / 2, grid.len() as i64 / 2), grid);
 }
